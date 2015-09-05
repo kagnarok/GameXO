bf3b6ee97b1ca9343f62ea2ece965cde7baa0a29
wittawus singlow <wittawus.s@gmail.com>
2015-09-05 19:10:13 +0700

    First Project created

---
+++ GameXO/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private GAMESTATE m_gameState;
+	// Use this for initialization
+	void Awake () {
+		InitGame();
+	}
+	
+	private void InitGame()
+	{
+		SetGameState(GAMESTATE.INIT);
+		CreateBoardGameWithSize(BOARDSIZE.S3X3);
+		CreatePlayerData();
+		RandomUserFirstPlay();
+		CreateUIForFirstPlayerChooseXOrO();
+	}
+
+	private void SetGameState(GAMESTATE state)
+	{
+		m_gameState = state;
+	}
+
+	private void CreateBoardGameWithSize(BOARDSIZE size)
+	{
+
+	}
+
+	private void CreatePlayerData()
+	{
+	}
+
+	private void RandomUserFirstPlay()
+	{
+	}
+
+	private void CreateUIForFirstPlayerChooseXOrO()
+	{
+
+	}
+}
